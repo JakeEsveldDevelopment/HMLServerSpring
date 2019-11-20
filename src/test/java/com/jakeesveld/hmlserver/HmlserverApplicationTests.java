52867e1b8a76afe6e6bb487cd0b820293a29d6f8
Jake Esveld <thejakeesveld@gmail.com>
2019-11-20 11:59:34 -0700

    Initial commit

---
+++ src/test/java/com/jakeesveld/hmlserver/HmlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakeesveld.hmlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmlserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
